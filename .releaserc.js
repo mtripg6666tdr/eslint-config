20090e33c96254de4fa512855efcacb0b145f2c4
うさみょん(myon2019)/mtripg6666tdr <56076195+mtripg6666tdr@users.noreply.github.com>
2023-05-29 23:53:08 +0900

    release config

--- .releaserc.js
+++ .releaserc.js
@@ -43,7 +43,7 @@ module.exports = {
           },
           {
             type: "Update",
-            release: false
+            release: "patch"
           },
           {
             type: "Add",
